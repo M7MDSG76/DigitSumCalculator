d1d011d0deb9870d643f2cf3b49cfcc2d7f23153
M7MDSG76 <mohammed.s.alghanmi@gmail.com>
2021-12-31 20:55:34 +0300

    Add project files.

---
+++ DIGITCOUNT/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DIGITCOUNT
+{
+    class Program
+    {
+        static int getSum(int n)
+        {
+            if (n == 0)
+                return 0;
+            Console.WriteLine(n);
+            return (n % 10 + getSum(n / 10));
+            
+
+          
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number of digit: ");
+            int num = int.Parse(Console.ReadLine());
+            int result = getSum(num);
+            Console.WriteLine("the sum of digit in " + num + " is " + result);
+        }
+    }
+}
